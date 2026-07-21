0f43695c97309cfbfdd3ec4c37b18323f32f33d7
KEv-177 <kvega9053@gmail.com>
2026-07-21 14:08:34 -0500

    Add files via upload

--- src/Paralelepipedo.java
+++ src/Paralelepipedo.java
@@ -1,24 +1,42 @@
-public class Paralelepipedo extends Figura3D{
-    private double base;
+class Paralelepido extends Punto {
+    private double largo;
+    private double ancho;
     private double altura;
 
-    public Paralelepipedo(double base , double altura){
-        this.base = base ;
-        this.altura= altura;
-    }
-
-
-
-    public double getAltura() {
-        return altura;
-    }
-
-    public void setAltura(double altura) {
-        this.altura = altura;
+    @Override
+    public void LeerDatos(Scanner scanner) {
+        super.LeerDatos(scanner);
+        System.out.print("Ingrese el largo: ");
+        this.largo = scanner.nextDouble();
+        System.out.print("Ingrese el ancho: ");
+        this.ancho = scanner.nextDouble();
+        System.out.print("Ingrese la altura: ");
+        this.altura = scanner.nextDouble();
     }
 
     @Override
-    public void calcularArea() {
+    public double calcularPerimetro() {
+        return 4 * (largo + ancho + altura);
+    }
 
+    @Override
+    public double calcularArea() {
+        return 2 * ((largo * ancho) + (largo * altura) + (ancho * altura));
+    }
+
+    @Override
+    public double calcularVolumen() {
+        return largo * ancho * altura;
+    }
+
+    @Override
+    public void imprimirAtributos() {
+        System.out.println("--- FIGURA: PARALELEPÍPEDO ---");
+        super.imprimirAtributos();
+        System.out.println("Largo: " + largo + " | Ancho: " + ancho + " | Altura: " + altura);
+        System.out.println("Perímetro: " + calcularPerimetro());
+        System.out.println("Área Total: " + calcularArea());
+        System.out.println("Volumen: " + calcularVolumen());
     }
 }
+
