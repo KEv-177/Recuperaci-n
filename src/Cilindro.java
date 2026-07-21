b039bffc1aae625efc5c4c3914de7b7db8445c5d
KEv-177 <kvega9053@gmail.com>
2026-07-21 14:10:23 -0500

    Add files via upload

--- src/Cilindro.java
+++ src/Cilindro.java
@@ -1,36 +1,32 @@
-public class Cilindro extends Figura{
+class Cilindro extends Punto {
     private double radio;
-    private  double altura;
+    private double altura;
 
-    public Cilindro(double radio, double altura){
-        this.radio = radio;
-        this.altura = altura;
-
-    }
-
-    public double getRadio() {
-        return radio;
-    }
-
-    public void setRadio(double radio) {
-        this.radio = radio;
-    }
-
-    public double getAltura() {
-        return altura;
-    }
-
-    public void setAltura(double altura) {
-        this.altura = altura;
+    @Override
+    public void LeerDatos(Scanner scanner) {
+        super.LeerDatos(scanner);
+        System.out.print("Ingrese el radio de la base: ");
+        this.radio = scanner.nextDouble();
+        System.out.print("Ingrese la altura: ");
+        this.altura = scanner.nextDouble();
     }
 
     @Override
-    public void calcularArea() {
-        this.Area = ((double)2.0F * this.radio * Math.PI * this.altura) + ((double)2.0F * Math.PI * this.radio);
+    public double calcularArea() {
+        return (2 * Math.PI * radio * altura) + (2 * Math.PI * Math.pow(radio, 2));
     }
 
     @Override
-    public void calcularvolumen() {
-        this.Volumen = Math.PI * this.radio * this.altura;
+    public double calcularVolumen() {
+        return Math.PI * Math.pow(radio, 2) * altura;
+    }
+
+    @Override
+    public void imprimirAtributos() {
+        System.out.println("--- FIGURA: CILINDRO ---");
+        super.imprimirAtributos();
+        System.out.println("Radio: " + radio + " | Altura: " + altura);
+        System.out.println("Área Total: " + calcularArea());
+        System.out.println("Volumen: " + calcularVolumen());
     }
 }
